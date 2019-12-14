7dc5d440f2bb9224239b5cd5cc7a247bde6c47a3
Przemko95 Konieczniak <przemyslaw.konieczniak1@outlook.com>
2019-12-14 18:58:44 +0000

    4-day/2

--- advent-of-code/4/index.js
+++ advent-of-code/4/index.js
@@ -1,8 +1,9 @@
 module.exports = function isValid (password) {
   if (password.length !== 6) return false
-  if (allDigitsAreTheSame(password)) return true
-  if (hasDouble(password) && hasCorrectOrder(password)) return true
-  return false
+  if (allDigitsAreTheSame(password)) return false
+  if (!hasCorrectOrder(password)) return false
+  if (!hasDouble(password)) return false
+  return true
 }
 
 function allDigitsAreTheSame (password) {
@@ -10,18 +11,34 @@ function allDigitsAreTheSame (password) {
   return digit.repeat(6) === password
 }
 
-function hasDouble (password) {
-  const digits = [...new Set(password.match(/[0-9]/g))]
-  for (let i = 0; i < digits.length; i++) {
-    const digit = digits[i]
-    const regexp = new RegExp(`${digit}${digit}+`, 'g')
-    if (password.match(regexp)) return true
-  }
-  return false
-}
-
 function hasCorrectOrder (password) {
   password = password.split('').map(char => Number(char))
   const passwordCopy = password.slice().sort()
-  return password.toString().replace(/,/g, '') === password.toString().replace(/,/g, '')
+  return password.toString().replace(/,/g, '') === passwordCopy.toString().replace(/,/g, '')
+}
+
+function hasDouble (password) {
+  const matches = getRepeatedDigits(password)
+  if (!matches.length) return false
+  if (hasOneDouble(matches)) return true
+  return false
+}
+
+function getRepeatedDigits (password) {
+  const digits = [...new Set(password.match(/[0-9]/g))]
+  const matches = []
+  for (let i = 0; i < digits.length; i++) {
+    const digit = digits[i]
+    const regexp = new RegExp(`${digit}{2,}`, 'g')
+    const match = password.match(regexp)
+    if (match) matches.push(match)
+  }
+  return matches
+}
+
+function hasOneDouble (matches) {
+  return matches.find(match => {
+    const sequence = match[0]
+    return sequence.length === 2
+  })
 }
--- advent-of-code/4/test.js
+++ advent-of-code/4/test.js
@@ -2,7 +2,7 @@ const assert = require('assert')
 const isValid = require('./index')
 
 const password1 = '111111'
-assert.deepEqual(isValid(password1), true)
+assert.deepEqual(isValid(password1), false)
 
 const password2 = '223450'
 assert.deepEqual(isValid(password2), false)
@@ -11,18 +11,61 @@ const password3 = '123789'
 assert.deepEqual(isValid(password3), false)
 
 const password4 = '111123'
-assert.deepEqual(isValid(password4), true)
+assert.deepEqual(isValid(password4), false)
 
 const password5 = '135679'
 assert.deepEqual(isValid(password5), false)
+
+const password6 = '123444'
+assert.deepEqual(isValid(password6), false)
+
+const password7 = '112233'
+assert.deepEqual(isValid(password7), true)
+
+const password8 = '111122'
+assert.deepEqual(isValid(password8), true)
+
+const password9 = '357778'
+assert.deepEqual(isValid(password9), false)
+
+const password10 = '444556'
+assert.deepEqual(isValid(password10), true)
+
+const password11 = '889999'
+assert.deepEqual(isValid(password11), true)
+
+const password12 = '127777'
+assert.deepEqual(isValid(password12), false)
+
+const password13 = '111123'
+assert.deepEqual(isValid(password13), false)
+
+const password14 = '124444'
+assert.deepEqual(isValid(password14), false)
+
+const password15 = '122235'
+assert.deepEqual(isValid(password15), false)
+
+const password16 = '112233'
+assert.deepEqual(isValid(password16), true)
+
+const password17 = '111223'
+assert.deepEqual(isValid(password17), true)
+
+const password18 = '788999'
+assert.deepEqual(isValid(password18), true)
+
+const password19 = '555789'
+assert.deepEqual(isValid(password19), false)
 
 function findPasswords () {
   let found = 0
   for (let i = 357253; i < 892942; i++) {
     const password = i.toString()
-    if (isValid(password)) found += 1
+    if (isValid(password)) { 
+      found += 1
+    }
   }
   return found
 }
-assert.deepEqual(findPasswords(), 530)
-
+assert.deepEqual(findPasswords(), 324)

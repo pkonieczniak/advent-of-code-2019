847d406f140cc287d4365181b611d25c6bc37c63
Przemko95 Konieczniak <przemyslaw.konieczniak1@outlook.com>
2019-12-17 13:48:51 +0000

    6-day/refactor(recurrsion)

--- advent-of-code/6/index.js
+++ advent-of-code/6/index.js
@@ -1,56 +1,58 @@
-module.exports = function getOrbits (map) {
-  const nodes = {}
-  map.forEach(relation => {
-    const [node1, node2] = relation.split(')')
-    if (!(nodes[node1])) {
-      nodes[node1] = {}
-    }
-    if (!(nodes[node2])) { 
-      nodes[node2] = {} 
-    }
-    nodes[node1][node2] = nodes[node2]
-  }) 
-  let total = { result: 0 }
-  walk(nodes, total)
-  function walk (nodes, total = {}) {
-    for (let node in nodes) {
-      const children = Object.keys(nodes[node])
-      total.result += children.length   
-      walk(nodes[node], total)
-    }
-  } 
-  const YOU_ORBIT = Object.keys(nodes).find(key => {
-    return Object.keys(nodes[key]).includes('YOU')
-  })
-  const SANTA_ORBIT = Object.keys(nodes).find(key => {
-    return Object.keys(nodes[key]).includes('SAN')
-  })
-  let counter = { counter: 0 }
-  const parents1 = []
-  const parents2 = []
-  function find (node, parents) {
-    for (let key in nodes) {
-      if (Object.keys(nodes[key]).includes(node)) {
-        parents.push({ key, depth: counter.counter += 1 })
-        // counter.counter += 1
-        find(key, parents)
-      }
-    }
-  }
-  find(SANTA_ORBIT, parents1)
-  counter.counter = 0
-  find(YOU_ORBIT, parents2)
-  const common_parents = [...intersection(parents1, parents2)]
+
+module.exports = function solve (universalOrbitMap) {
+  const graph = parse(universalOrbitMap)
+  const edges = countEdges(graph)
+  const [YOU_ORBIT, SANTA_ORBIT] = ['YOU', 'SAN'].map(element => findClosestParent(graph, element))
+  const commonAncestors = intersection(getAllAncestor(graph, YOU_ORBIT), getAllAncestor(graph, SANTA_ORBIT))
+  return { edges, commonAncestors }
 }
 
-function intersection (a, b) {
-  const common_keys = []
-  for (let i = 0; i < a.length; i++) {
-    for (let j = 0; j < b.length; j++) {
-      if (a[i].key === b[j].key) {
-        common_keys.push({ key: a[i].key, depth: a[i].depth + b[j].depth })
+function parse (universalOrbitMap) {
+  const graph = {}
+  universalOrbitMap.forEach(relation => {
+    const [node1, node2] = relation.split(')')
+    if (!(graph[node1])) graph[node1] = {}
+    if (!(graph[node2])) graph[node2] = {} 
+    graph[node1][node2] = graph[node2]
+  })
+  return graph
+}
+
+function countEdges (graph, total = 0) {
+  return Object.keys(graph).reduce((total, currentValue) => {
+    return countEdges(graph[currentValue], total + Object.keys(graph[currentValue]).length)
+  }, total)
+}
+
+function findClosestParent (graph, node) {
+  return Object.keys(graph).find(key => {
+    return Object.keys(graph[key]).includes(node)
+  })
+}
+
+function getAllAncestor (graph, node, depth = 0, accumulator = []) {
+  const nodes = Object.keys(graph)
+  return nodes.reduce((accumulator, key) => {
+    if (Object.keys(graph[key]).includes(node)) { 
+      depth += 1
+      accumulator.push({ key, depth }) 
+      return getAllAncestor(graph, key, depth, accumulator)
+    }
+    return accumulator
+  }, accumulator)
+}
+
+function intersection (parents1, parents2) {
+  const commonAncestors = []
+  for (let i = 0; i < parents1.length; i++) {
+    for (let j = 0; j < parents2.length; j++) {
+      if (parents1[i].key === parents2[j].key) {
+        const key = parents1[i].key
+        const depth = parents1[i].depth + parents2[j].depth
+        commonAncestors.push({ key, depth })
       }
     }
   }
-  return common_keys
+  commonAncestors.sort((a, b) => a.depth - b.depth)
+  return commonAncestors
 }
--- advent-of-code/6/test.js
+++ advent-of-code/6/test.js
@@ -1,17 +1,18 @@
-const getOrbits = require('.')
+const solve = require('.')
 const assert = require('assert')
 const fs = require('fs')
 const path = require('path')
 
-// const map1 = 'COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L'.split(',')
-// // assert.deepEqual(getOrbits(map1), 42)
+const map1 = 'COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L'.split(',')
+assert.deepEqual(solve(map1).edges, 42)
 
-// const map2 = 'COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L,K)YOU,I)SAN'.split(',')
-// getOrbits(map2)
-// assert.deepEqual(getOrbits(map2), 42)
+const map2 = 'COM)B,B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L,K)YOU,I)SAN'.split(',')
+assert.deepEqual(solve(map2).edges, 54)
 
 fs.readFile(path.join(__dirname, 'input.txt'), 'utf8', (err, data) => {
   if (err) throw(err)
-  const map2 = data.split('\n')
-  assert.deepEqual(getOrbits(map2).total, 314247)
+  const map3 = data.split('\n')
+  const { edges, commonAncestors } = solve(map3)
+  assert.deepEqual(edges, 314247)
+  assert.deepEqual(commonAncestors[0].depth, 514)
 })
